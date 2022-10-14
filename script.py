b39dea58462896eba3d8955182b63e5137d5874e
0xnirvana <nirvana.ctf@gmail.com>
2022-10-14 10:41:19 -0700

    process_publish added

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import os
+import re
 
 host = "https://api.medium.com/v1"
 
@@ -27,6 +29,56 @@ def check_token():
     print("ID Found: " + id)
     return token, id
 
+def process_publish(token, id):
+    publish_url = host + "/users/{}/posts".format(id)
+    headers = {'Authorization': 'Bearer {}'.format(token),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'}
+
+    md_files = {}
+    index = 1
+    platform = input("Enter the platform (HTB/THM/...):\n")
+    tags = input("Enter comma separated tags:\n")
+    tags = tags.split(",")
+    full_path = input("Enter the full path of the writeup directory:\n")
+    
+    for root, dirs, files in os.walk(os.path.abspath(full_path)):
+        for file in files:
+            if file.endswith('.md'):
+                md_files[index] = os.path.join(root, file)
+                index += 1
+    print("Markdown files detected in the directory:")
+    
+    for key,values in md_files.items():
+        print("[{}]\t\t{}".format(key, values))
+    file_number = input("Enter the index value of the file that contains your writeup:\n")
+    file_number = int(file_number)
+
+    content = open(md_files[file_number], "r").read()
+    content = add_display_image(content)
+    # print(content)
+    title = platform + ": " + re.findall('^\#\s(.+?)\n', content)[0]
+
+    content = replace_internal_image_links(content, md_files[file_number])
+    cont = input("Press enter if you want to continue to publish the file.")
+
+    if cont != "":
+        print("Exiting without publishing the article")
+        exit()
+
+    data = {
+        "title": title,
+        "contentFormat": "markdown",
+        "content": content,
+        "tags": tags,
+        "publishStatus": "public"
+    }
+
+    post_response_raw = requests.post(publish_url, headers=headers, json=data)
+    post_response = json.loads(post_response_raw.content)
+    print (json.dumps(post_response, indent=4))
+
+    return 1
 
 if __name__ == '__main__':
     token, id = check_token()
